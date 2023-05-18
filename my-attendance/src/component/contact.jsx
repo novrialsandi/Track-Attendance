b32f35228aa77f03cabb48de2c0161dccd02fddf
Prekksu <novrialsandi@gmail.com>
2023-05-18 09:48:17 +0700

    p

--- my-attendance/src/component/contact.jsx
+++ my-attendance/src/component/contact.jsx
@@ -3,14 +3,13 @@ import { Box, Center } from "@chakra-ui/react";
 
 export default function Contact() {
 	return (
-		<Center h={"100%"} w={"100%"}>
-			<Box
-				id="boxContact"
-				w={"600px"}
-				h={"700px"}
-				justifyContent={"center"}
-				alignItems={"center"}
-			></Box>
+		<Center
+			h={"100vh"}
+			w={"100vw"}
+			justifyContent={"center"}
+			alignItems={"center"}
+		>
+			<Box id="boxContact" w={"600px"} h={"700px"}></Box>
 		</Center>
 	);
 }
