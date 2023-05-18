8f45dc3f46c75c4d95c3d73d9a836a559d067191
Prekksu <novrialsandi@gmail.com>
2023-05-18 10:39:31 +0700

    p

--- my-attendance/src/component/contact.jsx
+++ my-attendance/src/component/contact.jsx
@@ -1,5 +1,5 @@
 import "../css/contact.css";
-import { Box, Center } from "@chakra-ui/react";
+import { Box, Center, InputGroup, Input } from "@chakra-ui/react";
 
 export default function Contact() {
 	return (
@@ -9,7 +9,46 @@ export default function Contact() {
 			justifyContent={"center"}
 			alignItems={"center"}
 		>
-			<Box id="boxContact" w={"600px"} h={"700px"}></Box>
+			<Box id="boxContact" w={"390px"} h={"844px"}>
+				<Center>Login</Center>
+				<Box>
+					<InputGroup
+						paddingLeft={"30px"}
+						paddingRight={"30px"}
+						display={"flex"}
+						flexDir={"column"}
+						justifyContent={"center"}
+						alignItems={"center"}
+					>
+						<Input
+							placeholder="Email/Username"
+							w={"80%"}
+							border={"1px solid #A5A5A5"}
+						></Input>
+						<Input
+							placeholder="Password"
+							w={"80%"}
+							border={"1px solid #A5A5A5"}
+						></Input>
+					</InputGroup>
+				</Box>
+				<Center>
+					<Center
+						w={"140px"}
+						h={"48px"}
+						borderRadius={"25px"}
+						color={"black"}
+						border={"1px solid #A5A5A5"}
+						bgColor={"rgb(173,173,173"}
+					>
+						LOGIN
+					</Center>
+				</Center>
+
+				<Center>Don't have an account?</Center>
+
+				<Center>SIGN UP</Center>
+			</Box>
 		</Center>
 	);
 }
