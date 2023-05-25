b78fa243b0cac5046da0a0c077e9da4471f1141d
Prekksu <novrialsandi@gmail.com>
2023-05-25 12:04:31 +0700

    Update config.js

--- Absensi APP/src/config/config.js
+++ Absensi APP/src/config/config.js
@@ -23,15 +23,15 @@
 // }
 
 const { db_username, db_password, db_database, db_host, db_dialect } =
-  process.env;
+	process.env;
 
 // console.log(db_database);
 module.exports = {
-  development: {
-    username: db_username,
-    password: db_password,
-    database: db_database,
-    host: db_host,
-    dialect: "mysql",
-  },
+	development: {
+		username: db_username,
+		password: db_password,
+		database: db_database,
+		host: db_host,
+		dialect: db_dialect,
+	},
 };
